d690cd6b53cd3ced3f993ee4b6ffd6c4e8873e33
tejth <ptejendra91@gamil.com>
2025-05-17 20:16:56 +0530

    created student  model

--- Home/Home/settings.py
+++ Home/Home/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "school",
+    "student"
 ]
 
 MIDDLEWARE = [
--- Home/Home/urls.py
+++ Home/Home/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("",include("school.urls"))
+    path("",include("school.urls")),
+    path("student/",include("student.urls"))
 ]
